97b15c79fc3cb9e42c7b75814f96151ea9be65c9
zakiya-abass <zakiyaabass@icloud.com>
2023-02-20 12:59:05 +0300

    react-hooks-event-handling-lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleInputChange = () => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <div>
+      <label>
+        Password:
+        <input type="password" onChange={handleInputChange} />
+      </label>
+    </div>
+  );
 }
 
 export default Keypad;
